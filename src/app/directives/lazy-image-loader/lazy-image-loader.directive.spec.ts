65e88e6148983133864383fe6085d6e55fe0f06c
Prathush <prathush.jalam@gmail.com>
2022-05-04 21:14:33 +0200

    added test for lazy loading directive

--- src/app/directives/lazy-image-loader/lazy-image-loader.directive.spec.ts
+++ src/app/directives/lazy-image-loader/lazy-image-loader.directive.spec.ts
@@ -1,8 +1,19 @@
+import { ElementRef } from '@angular/core';
 import { LazyImageLoaderDirective } from './lazy-image-loader.directive';
 
 describe('LazyImageLoaderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyImageLoaderDirective();
+
+  const elementRef = { nativeElement: { setAttribute: {} } } as ElementRef;
+  let directive;
+
+  beforeEach(() => {
+    spyOn(elementRef.nativeElement, 'setAttribute');
+    directive = new LazyImageLoaderDirective(elementRef);
+
+  });
+
+  it('should create an instance and set img attribute', () => {
     expect(directive).toBeTruthy();
+    expect(elementRef.nativeElement.setAttribute).toHaveBeenCalled();
   });
 });
